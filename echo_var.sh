079127067b537a03256468b5692481ea2a503471
PC-47 <32375595+PC-47@users.noreply.github.com>
2024-08-23 12:44:34 -0700

    Update echo_var.sh

--- echo_var.sh
+++ echo_var.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Output all environment variables for debugging
-env
+# env
 
 # Output the specific survey variable
 # echo "SEMAPHORE_string is set to: ${SEMAPHORE_string}"
 
-echo "survey string var is set to -> ${1}"
+for var in "${@}"; do
+  echo "${var}
+done
