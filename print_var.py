eec63d4d0faacfc106e47730d2d4c94377595e8e
PC-47 <32375595+PC-47@users.noreply.github.com>
2024-08-23 12:27:39 -0700

    Update print_var.py

--- print_var.py
+++ print_var.py
@@ -3,3 +3,9 @@ print("testing print var scriopt")
 var = "{{ survey_var }}"
 
 print(var)
+
+import os
+
+# Print all environment variables
+for key, value in os.environ.items():
+    print(f'{key}={value}')
